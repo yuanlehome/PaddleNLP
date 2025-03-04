ba6096dafa31a0bfd8fdf474f1b87e58e76d1d5b
lizhenyun01 <1500424927@qq.com>
2025-03-04 14:24:48 +0800

    search mla_chunk_size

--- paddlenlp/experimental/transformers/fused_transformer_layers.py
+++ paddlenlp/experimental/transformers/fused_transformer_layers.py
@@ -1514,6 +1514,7 @@ class FusedMultiTransformerBase(Layer):
                 kwargs["decoder_tile_ids_per_batch"],
                 kwargs["decoder_num_blocks"],
                 kwargs["decoder_num_blocks_cpu"],
+                kwargs["decoder_chunk_size"],
                 kwargs["max_len_kv"],
             ) = get_block_shape_and_split_kv_block(
                 kwargs.get("seq_lens_encoder", None),
@@ -3080,6 +3081,7 @@ class FusedBlockMultiTransformer(FusedMultiTransformerBase):
                 kwargs.get("decoder_tile_ids_per_batch", None),
                 kwargs.get("decoder_num_blocks", None),
                 kwargs.get("decoder_num_blocks_cpu", None),
+                kwargs.get("decoder_chunk_size", None),
                 kwargs.get("max_enc_len_this_time", None),
                 kwargs.get("max_dec_len_this_time", None),
                 kwargs.get("max_len_kv", None),
@@ -3435,6 +3437,7 @@ class FusedBlockMultiTransformerWeightOnly(FusedBlockMultiTransformer, FusedMult
                 kwargs.get("decoder_tile_ids_per_batch", None),
                 kwargs.get("decoder_num_blocks", None),
                 kwargs.get("decoder_num_blocks_cpu", None),
+                kwargs.get("decoder_chunk_size", None),
                 kwargs.get("max_enc_len_this_time", None),
                 kwargs.get("max_dec_len_this_time", None),
                 kwargs.get("max_len_kv", None),
